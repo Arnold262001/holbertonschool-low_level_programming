af75ca75d788a017250be0f33aabeae1b07c77a4
Arnold262001 <antoni_11_2001@hotmail.com>
2023-08-04 09:55:11 -0700

    Task 4 intento 1 Proyect: C-Hello World

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - punto de entrada
+ *
+ * Return: siempre 0
+ **/
+int main(void)
+{
+	    puts("Programming is like building a multilingual puzzle");
+	        return (0);
+}
