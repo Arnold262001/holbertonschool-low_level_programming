1907d72677e40812c4d31eb963d48c4fa577a3af
Arnold262001 <antoni_11_2001@hotmail.com>
2023-08-06 08:07:19 -0700

    Task 3 intento 2 Proyect: If, Else, While correcion de espacios

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,16 @@
 
 int main(void)
 {
-		int c;
-			int LF = 10;
+	int c;
+	int LF = 10;
 
-				for (c = 97 ; c <= 122 ; c++)
-							putchar(c);
+	for (c = 97 ; c <= 122 ; c++)
+		putchar(c);
 
-					for (c = 65 ; c <= 90 ; c++)
-								putchar(c);
+	for (c = 65 ; c <= 90 ; c++)
+		putchar(c);
 
-						putchar(LF);
-							getchar();
-								return (0);
+	putchar(LF);
+	getchar();
+	return (0);
 }
